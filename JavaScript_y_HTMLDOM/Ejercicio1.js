a33363a33fc9caf4b77a9d9045c69fd57631f2f6
Alvarosh04 <alvarosanchez1708@gmail.com>
2025-04-24 17:53:08 +0200

    Add files via upload

---
+++ JavaScript_y_HTMLDOM/Ejercicio1.js
@@ -0,0 +1,7 @@
+function cambiaparrafo() {
+    document.getElementById("demo").innerHTML = "El párrafo ha sido cambiado.";
+}
+
+function borraparrafo() {
+    document.getElementById("demo").style.display = "none"; // Cambiado a "none" para ocultar el párrafo
+}
---
+++ JavaScript_y_HTMLDOM/Ejercicio2.js
@@ -0,0 +1,11 @@
+function cambiaparrafo() {
+    document.getElementById("demo").innerHTML = "El párrafo ha sido cambiado.";
+}
+
+function borraparrafo() {
+    document.getElementById("demo").style.display = "none";
+}
+
+function escribirenparrafo() {
+    document.getElementById("demo").innerHTML = "¡Hola Mundo!";
+}
