8ac38f909866b1268a2d8f827fe1936c3838f8c7
Alvarosh04 <alvarosanchez1708@gmail.com>
2025-04-24 17:51:40 +0200

    Delete JavaScript y HTML DOM directory

--- JavaScript y HTML DOM/Ejercicio1.js
+++
@@ -1,7 +0,0 @@
-function cambiaparrafo() {
-    document.getElementById("demo").innerHTML = "El párrafo ha sido cambiado.";
-}
-
-function borraparrafo() {
-    document.getElementById("demo").style.display = "none"; // Cambiado a "none" para ocultar el párrafo
-}
